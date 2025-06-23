828abacfff2819a4122b0f951ef2d202567bfe65
Jorge Matos <jisapjisap@gmail.com>
2025-06-23 17:34:54 +0200

    Update favicon and enhance Navbar animation with GSAP
    - Changed the favicon from 'vite.svg' to 'logo.png' for better branding.
    - Implemented GSAP animations in the Navbar component to improve user experience during scrolling.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,26 @@
+import { useGSAP } from "@gsap/react"
 import { navLinks } from "../../constants"
+import gsap from "gsap"
 
 
 const Navbar = () => {
+
+  useGSAP(() => {
+    const navTween =gsap.timeline({
+      scrollTrigger: {                  
+        trigger: 'nav',               
+        start: 'bottom center'          // Cuando el bottom de la nav se encuentre en el centro de la pantalla se inicia la animación
+      }
+    })
+
+    navTween.fromTo('nav', { backgroundColor: 'transparent' }, {
+      backgroundColor: '#00000050',
+      backgroundFilter: 'blur(10px)',
+      duration: 1,
+      ease: 'power1.inOut'
+    })
+  },[])
+
   return (
     <nav>
       <div>
