fe4fd4bfa640cb00d31ba06905052c3157fab71f
Jorge Matos <jisapjisap@gmail.com>
2025-06-23 16:40:46 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+
+import { scrollTrigger, SplitText } from 'gsap/all'
+
+gsap.registerPlugin(ScrollTrigger, SplitText)
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-xl'>App</h1>
+    </div>
+  )
+}
+
+export default App
