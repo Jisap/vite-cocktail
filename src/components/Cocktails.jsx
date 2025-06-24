4b162c0ffe626b3654dcaf81d4086951f75844b2
Jorge Matos <jisapjisap@gmail.com>
2025-06-24 15:01:58 +0200

    Add Cocktails component and update App layout
    - Introduced a new Cocktails component to display popular cocktails and mocktails.
    - Updated the App component to include the Cocktails section.
    - Replaced the video source in the Hero component with the new output video file.

---
+++ src/components/Cocktails.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { cocktailLists, mockTailLists } from '../../constants'
+
+const Cocktails = () => {
+  return (
+    <section id="cocktails" className='noisy'>
+      <img 
+        src="/images/cocktail-left.png"
+        alt="l-leaf"
+        id="c-left-leaf"
+      />
+      <img 
+        src="/images/cocktail-right.png"
+        alt="r-leaf"
+        id="c-right-leaf"
+      />
+
+      <div className='list'>
+        <div className='popular'>
+          <h2 className=''>Most popular cocktails:</h2>
+          <ul>
+            {cocktailLists.map(({ name, country, detail, price }) => (
+              <li key={name}>
+              {/* me-28 aplica un margen al final de la dirección de la linea (derecha) solo si es un tamaño "md" */}
+                <div className='md:me-28'>
+                  <h3>{name}</h3>
+                  <p>{country} | {detail}</p>
+                </div>
+                <span>- {price}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className='loved'>
+          <h2 className=''>Most loved mocktails:</h2>
+          <ul>
+            {mockTailLists.map(({ name, country, detail, price }) => (
+              <li key={name}>
+                <div className='me-28'>
+                  <h3>{name}</h3>
+                  <p>{country} | {detail}</p>
+                </div>
+                <span>- {price}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Cocktails
