ea4f7450ca911dc3eb8f38016ecb57eaed791b97
Jorge Matos <jisapjisap@gmail.com>
2025-06-25 15:35:11 +0200

    Add GSAP animations to About component for enhanced scroll effects
    - Implemented GSAP scroll-triggered animations for the About section, animating the title and images as the user scrolls.
    - Utilized SplitText to create a dynamic entrance effect for the title words, enhancing visual engagement.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,38 @@
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import { SplitText } from 'gsap/all'
 import React from 'react'
 
 const About = () => {
+
+  useGSAP(() => {
+    const titleSplit = SplitText.create('#about h2', {  // 1. Divide el título en palabras
+      type: 'words'
+    })
+
+    const scrollTimeline = gsap.timeline({              // 2. Crea una "línea de tiempo" para las animaciones
+      scrollTrigger: {
+        trigger: '#about',                              // El activador es la sección "About"
+        start: 'top center',                            // La animación empieza cuando la parte superior de la sección llega al centro de la pantalla
+      }
+    })
+
+    scrollTimeline                                      // 3. Define la primera animación (el título)
+      .from(titleSplit.words, {                         // Anima cada palabra del título
+        opacity: 0,                                     // Empiezan siendo invisibles
+        duration: 1,                                    // La animación dura 1 segundo
+        yPercent: 100,                                  // Empiezan 100% por debajo de su posición final (efecto de subir)
+        ease: 'expo.out',                               // Una curva de aceleración suave para un efecto más natural
+        stagger: 0.02                                   // Cada palabra empieza 0.02s después de la anterior 
+      })
+      .from('.top-grid div, .bottom-grid div', {        // 4. Define la segunda animación (las imágenes) // Anima los contenedores de las imágenes
+        opacity: 0,                                     // Empiezan siendo invisibles
+        duration: 1,                                    // La animación dura 1 segundo
+        ease: 'power1.inOut',                           // Otra curva de aceleración
+        stagger: 0.04                                   // Cada imagen empieza 0.04s después de la anterior
+      }, '-=0.5')                                       // 5. Control de tiempo: empieza 0.5s ANTES de que la animación anterior termine 
+  },[])
+
   return (
     <div id="about">
       <div className='mb-16 md:px-0 px-5'>
