6cfef72ad07a079ab2a3d563b2ab01a46fa7198c
Jorge Matos <jisapjisap@gmail.com>
2025-06-27 09:58:25 +0200

    Add GSAP animations to Menu component for enhanced cocktail navigation
    - Implemented GSAP animations to improve the transition effects when navigating between cocktails.
    - Added logic to handle animation direction based on the current and previous cocktail indices.
    - Updated the Menu component to utilize useRef for managing the previous index state, ensuring smooth animations during transitions.

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,12 +2,47 @@
 
 import React, { useRef, useState } from 'react'
 import { sliderLists } from '../../constants'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 
 const Menu = () => {
 
-  const contentRef =useRef()
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const contentRef =useRef()                           // Usar el valor de current no provoca un nuevo renderizado
+  const [currentIndex, setCurrentIndex] = useState(0); // Estado del índice actual que solo se jecuta la primera vez
+  const prevIndexRef = useRef(currentIndex);           // Se establece prevIndex en 0 la primera vez
   const totalCocktails = sliderLists.length;
+
+  useGSAP(() => {
+
+    const previousIndex = prevIndexRef.current;  // Se guarda el índice actual de forma persistente.
+
+    gsap.fromTo("#title", 
+      { opacity: 0 }, 
+      { opacity: 1, duration: 1 }
+    )
+   
+    let direction = currentIndex > previousIndex ? 1 : -1;                    // Determina el sentido de la animación. 1 derecha, -1 izquierda.
+    if (previousIndex === totalCocktails - 1 && currentIndex === 0) {         // Del último al primero
+      direction = 1;
+    } else if (previousIndex === 0 && currentIndex === totalCocktails - 1) { // Del primero al último
+      direction = -1;
+    }
+
+    gsap.fromTo(".cocktail img", 
+      { opacity: 0, xPercent: -100 * direction},
+      { opacity: 1, xPercent: 0, duration: 1, ease: "power1.inOut"},
+    )
+    gsap.fromTo(".details h2 ",
+      { yPercent: 100, opacity: 0 },
+      { yPercent: 0, opacity: 100, ease: "power1.inOut" } 
+    )
+    gsap.fromTo(".details p ",
+      { yPercent: 100, opacity: 0 },
+      { yPercent: 0, opacity: 100, ease: "power1.inOut" } 
+    )
+    
+    prevIndexRef.current = currentIndex;                                   // Actualiza la referencia del índice previo para la siguiente animación
+  },[currentIndex]);
 
   const goToSlide = (index) => {
     const newIndex = (index + totalCocktails) % totalCocktails;
@@ -21,7 +56,6 @@ const Menu = () => {
   const currentCocktail = getCocktailAt(0)
   const nextCocktail = getCocktailAt(1)
   const prevCocktail = getCocktailAt(-1)
-
 
   return (
     <section id="menu" aria-labelledby="menu-heading">
@@ -59,7 +93,7 @@ const Menu = () => {
         <div className='arrows'>
           <button
             className='text-left'
-            onClick={() => goToSlide(currentIndex - 1)}
+            onClick={() => goToSlide(currentIndex - 1)} 
           >
             <span>{prevCocktail.name}</span>
             <img 
@@ -70,7 +104,7 @@ const Menu = () => {
           </button>
           <button
             className='text-left'
-            onClick={() => goToSlide(currentIndex + 1)}
+            onClick={() => goToSlide(currentIndex + 1)} // currentIndex se actualiza -> nueva renderización -> se ejecuta la animación con el prevIndex anterior -> se actualiza el prevIndex
           >
             <span>{nextCocktail.name}</span>
             <img 
