553d185427819e2d0f69e4be92dc878dba853efd
Jorge Matos <jisapjisap@gmail.com>
2025-06-27 13:06:35 +0200

    Add Contact component to enhance user engagement
    - Introduced a new Contact component that provides users with location, contact information, opening hours, and social media links.
    - Updated the App component to include the Contact section for improved accessibility and user interaction.

---
+++ src/components/Contact.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import { openingHours, socials } from '../../constants'
+
+const Contact = () => {
+  return (
+    <footer id="contact">
+      <img 
+        src="/images/footer-right-leaf.png"
+        alt="leaf-right"
+        id="f-right-leaf"
+      />
+      <img 
+        src="/images/footer-left-leaf.png"
+        alt="leaf-left"
+        id="f-left-leaf"
+      />
+
+      <div className='content'>
+        <h2>Where to Find Us</h2>
+
+        <div>
+          <h3>Visit Our Bar</h3>
+          <p>456, Raq Blvd. #404, Los Angeles, CA 90045</p>
+        </div>
+
+        <div>
+          <h3>Contact Us</h3>
+          <p>(555) 987-6543</p>
+        </div>
+
+        <div>
+          <h3>Open Everyday</h3>
+          {openingHours.map((time) => (
+            <p key={time.day}>
+              {time.day} : {time.time}
+            </p>
+          ))}
+        </div>
+
+        <div>
+          <h3>Socials</h3>
+          <div className='flex-center gap-5'>
+            {socials.map((social) => (
+              <a 
+                key={social.name} 
+                href={social.url} 
+                target='_blank'            // open in new tab
+                rel='noopener noreferrer'  // Evita pasar la información de referencia al sitio web de destino al eliminar la información de referencia del encabezado HTTP.
+                aria-label={social.name}
+              >
+                <img src={social.icon} alt={social.name} />
+              </a>
+            ))}
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Contact
